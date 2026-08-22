8d1a0743fa1f0e603507af077389eee0811fc51f
markxzyz <77148071+markxzyz@users.noreply.github.com>
2026-08-22 07:22:32 +0000

    task2

---
+++ Task2/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+       
+        byte zero = 0;
+        short threeEleven = 3110;
+        int one = 1;
+        float twoPointZero = 2.0f;
+        char h = 'H';
+        boolean isTrue = true;
+
+        
+        String output = h + "" + threeEleven + " w" + zero + "r" + one + "d " + twoPointZero + " " + isTrue;
+
+      
+        System.out.println(output);
+    }
+}
