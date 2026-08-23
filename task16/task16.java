316a9734bd779fa25318556ef13eeee721b0f65d
markxzyz <77148071+markxzyz@users.noreply.github.com>
2026-08-23 14:59:27 +0000

    task16

---
+++ task16/task16.java
@@ -0,0 +1,20 @@
+package task16;
+
+class Gorilla implements Animal {
+
+    @Override
+    public boolean feed(boolean timeToEat) {
+       
+        return timeToEat;
+    }
+
+    @Override
+    public void groom() {
+        
+    }
+
+    @Override
+    public void pet() {
+       
+    }
+}
